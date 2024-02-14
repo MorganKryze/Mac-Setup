7259f5b864fc21a7868ab5edecdd6f1816adea3f
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-02-14 20:15:22 +0000

    🚑 update script execution

--- src/install.sh
+++ src/install.sh
@@ -14,9 +14,9 @@ brew upgrade
 echo "Installing Classic tools."
 brew install raycast maccy arc notion thunderbird pomatez dropzone cheatsheet imageoptim vlc drawio inkscape canva bitwarden lulu clamav onyx appcleaner monitorcontrol stats devtoys only-switch
 
-read -r is_developer"?Do you want to install developer tools? (y/n): "
+read -r is_developer"?Do you want to install developer tools? (y/n): " </dev/tty
 if [[ $is_developer == "y" || $is_developer == "Y" ]]; then
-    read -r manage_tiling"?Do you want to manage window tiling manually? (y/n): "
+    read -r manage_tiling"?Do you want to manage window tiling manually? (y/n): " </dev/tty
     if [[ $manage_tiling == "y" || $manage_tiling == "Y" ]]; then
         brew install rectangle
     else
